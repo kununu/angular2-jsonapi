34b6bc14dfef4ff94846cfee958a409b2bd49af2
João Magalhães <joao.magalhaes@kununu.com>
2020-02-24 14:46:44 +0000

    v8.0.1

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
+tslib_1.__exportStar(require("./converters/json-model/json-model.converter"), exports);
+tslib_1.__exportStar(require("./decorators/has-many.decorator"), exports);
+tslib_1.__exportStar(require("./decorators/belongs-to.decorator"), exports);
+tslib_1.__exportStar(require("./decorators/attribute.decorator"), exports);
+tslib_1.__exportStar(require("./decorators/nested-attribute.decorator"), exports);
+tslib_1.__exportStar(require("./decorators/json-attribute.decorator"), exports);
+tslib_1.__exportStar(require("./decorators/json-api-model-config.decorator"), exports);
+tslib_1.__exportStar(require("./decorators/json-api-datastore-config.decorator"), exports);
+tslib_1.__exportStar(require("./models/json-api-meta.model"), exports);
+tslib_1.__exportStar(require("./models/json-api.model"), exports);
+tslib_1.__exportStar(require("./models/json-nested.model"), exports);
+tslib_1.__exportStar(require("./models/error-response.model"), exports);
+tslib_1.__exportStar(require("./models/json-api-query-data"), exports);
+tslib_1.__exportStar(require("./providers"), exports);
+tslib_1.__exportStar(require("./module"), exports);
+//# sourceMappingURL=index.js.map
---
+++ dist/providers.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
+var json_api_datastore_service_1 = require("./services/json-api-datastore.service");
+tslib_1.__exportStar(require("./services/json-api-datastore.service"), exports);
+exports.PROVIDERS = [
+    json_api_datastore_service_1.JsonApiDatastore
+];
+//# sourceMappingURL=providers.js.map
